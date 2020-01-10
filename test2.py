d27ad812b4448fab6614a91b288974c0e35d53e8
bjornapermeti <50014113+bjornapermeti@users.noreply.github.com>
2020-01-10 15:54:22 +0000

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-content = 'prova 2'
-mail = smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login('bjornabooks@gmail.com','bjorna123')
-mail.sendmail('bjornabooks@gmail.com','bjornabooks@gmail.com', content)
-mail.close()
